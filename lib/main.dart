25133db06b2121d0255a14c3559fc459c605f463
Marcos <marcosbarcelar1@gmail.com>
2023-11-23 16:33:50 -0300

    Widgets: Container, Stack, Column, Row, ElevatedButton, Text e  OverFlow

--- lib/main.dart
+++ lib/main.dart
@@ -10,58 +10,71 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+        home: Container(
+          color: Colors.white,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Container(
+                    color: Colors.red,
+                    width: 100,
+                    height: 100,
+                  ),
+                  Container(
+                    color: Colors.blue,
+                    width: 50,
+                    height: 50,
+                  ),
+                ],
+              ),
+              Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Container(
+                    color: Colors.red,
+                    width: 100,
+                    height: 100,
+                  ),
+                  Container(
+                    color: Colors.blue,
+                    width: 50,
+                    height: 50,
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(color: Colors.cyan, height: 50, width: 50),
+                  Container(color: Colors.pinkAccent, height: 50, width: 50),
+                  Container(color: Colors.purple, height: 50, width: 50),
+                ],
+              ),
+              Container(
+                color: Colors.amber,
+                height: 300,
+                width: 300,
+                child: const Text(
+                  'Diamante Amarelo',
+                  style: TextStyle(color: Colors.black, fontSize: 28),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              ElevatedButton(onPressed: (){
+                print('Você apertou o botão');
+                },child: Text('Aperte o meu butão'),)
+            ],
+          ),
+        )
     );
   }
 }
